bf2c22b3f9588515159e8cd1deb52ce2074840a6
Vgenesin <Valentina.genesini@edu.unife.it>
2024-05-03 16:38:23 +0200

    update likelihood
    Updated self. Now should be clear which parameters we have to import into the likelihood

--- elica/likelihood.py
+++ elica/likelihood.py
@@ -8,20 +8,33 @@ class Elica(Likelihood):
 
     def __init__(
         self,
-        name: str = None,
         datafile: str = None,
-        lmin: int = 2,
-        lmax: int = 29,
-    ):	
+    ):
+
+    self.lmin
+    self.lmax
+    self.nsims
+    self.nsp
+
+    self.offset
+    self.fiducial
+
+    self.Cldata
+    self.covariance
+
+    self.covariance = np.linalg.inv(self.covariance)
 
     def get_requirements(self):
-        return {'Cl': {'ee': self.lmax}}
+        return {'Cl': {'ee': self.lmin, self.lmax}}
 
     def log_likelihood(self, cls_EE):
         r"""
 		Test
         """
-        
+        Clth = np.tile(cls_EE,self.nsp)
+
+
+
     def logp(self, **params_values):
         cls = self.provider.get_Cl(ell_factor=True)['ee']
         return self.log_likelihood(cls)
