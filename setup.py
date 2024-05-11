243368f2c0d271959caa172f92482fcb0c7f2f45
Giacomo Galloni <giacomo.galloni@unife.it>
2024-05-11 15:32:39 +0200

    Test pre-commit

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
+from __future__ import annotations
+
 import os
-import time
 
 from setuptools import setup
 
 file_dir = os.path.abspath(os.path.dirname(__file__))
 os.chdir(file_dir)
 
+
 setup(
     name="elica",
     version="0.1",
-    description="E-mode Likelihood with Cross-correlation Analysis (ELiCA): external Cobaya likelihood package",
-    zip_safe=False,  # set to false if you want to easily access bundled package data files
+    description="E-mode Likelihood with Cross-correlation Analysis (ELiCA):"
+    "external Cobaya likelihood package",
+    zip_safe=False,
     packages=["elica", "elica.tests"],
     package_data={
         "elica": [
