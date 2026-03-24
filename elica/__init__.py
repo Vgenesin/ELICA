7d735801ca25e38245902065b50ce9d03c49c86f
Vgenesin <Valentina.genesini@edu.unife.it>
2026-03-24 17:17:10 +0100

    updating init.py
    Adding single field likelihood. Both auto and cross spectra

--- elica/__init__.py
+++ elica/__init__.py
@@ -1,37 +1,28 @@
 from .likelihood import (
+    EE_100x100,
     EE_100x143,
-    EE_100x143_100xWL,
-    EE_100x143_100xWL_143xWL,
-    EE_100x143_143xWL,
-    EE_100x143_WLxWL,
     EE_100xWL,
+    EE_143x143,
     EE_143xWL,
     EE_full,
     EE_WLxWL,
-    EE_old100x143,
-    EE_oldWLxWL,
 )
 from .likelihood import mHL
 from .likelihood import hybridHL
-
 from .likelihood import fullHL
 
 
 __all__ = [
+    "EE_100x100",
     "EE_100x143",
     "EE_100xWL",
+    "EE_143x143",
     "EE_143xWL",
     "EE_WLxWL",
-    "EE_100x143_100xWL",
-    "EE_100x143_143xWL",
-    "EE_100x143_WLxWL",
-    "EE_100x143_100xWL_143xWL",
     "EE_full",
     "mHL",
     "hybridHL",
     "fullHL",
-    "EE_old100x143",
-    "EE_oldWLxWL",
 
 ]
 
