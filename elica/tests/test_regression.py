a260d4368abb35474a757bb586c00629d32f00be
Giacomo Galloni <giacomo.galloni@unife.it>
2026-03-26 10:57:29 +0100

    Add regression tests for all single-field and elica likelihoods

---
+++ elica/tests/test_regression.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pytest
+from cobaya.model import get_model
+
+from elica.tests.conftest import make_cobaya_info
+
+REFERENCE_VALUES = {
+    "EE_100x100": -1.694695323446801e01,
+    "EE_100x143": -1.182303223078453e01,
+    "EE_100xWL": -1.248755432596433e01,
+    "EE_143x143": -1.358281344335101e01,
+    "EE_143xWL": -1.280549327691968e01,
+    "EE_WLxWL": -6.984066728707005e00,
+    "elica": -6.000384701833366e01,
+}
+
+
+@pytest.mark.parametrize(
+    "likelihood_name, expected_loglike",
+    list(REFERENCE_VALUES.items()),
+    ids=list(REFERENCE_VALUES.keys()),
+)
+def test_loglike_at_tau_0060(likelihood_name, expected_loglike):
+    info = make_cobaya_info(likelihood_name)
+    model = get_model(info)
+    loglike = model.loglikes({})[0][0]
+    assert np.isclose(loglike, expected_loglike, atol=1e-6), (
+        f"{likelihood_name}: got {loglike}, expected {expected_loglike}"
+    )
